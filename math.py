d4419038bc8ba5d25dcc0c9c730023b051b431d3
bhushanyeshe <70462971+bhushanyeshe@users.noreply.github.com>
2021-03-04 11:33:49 +0530

    Add files via upload
    initial commited

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add implementation
+def add(x,y):
+add=x+y;
+return add;
+	
+
+# Subtract implementation
+def subtract(x,y):
+    
+
+# Multiply implementation
+def multiply(x,y):
+	
+
+# Divide implementation
+def divide(x,y):
+    
+log(x);
